097bf47eb71f5d36b4704ccdecda5ad41f45e3d1
Luis Fernando Romero 10 <320584622@pcpuma.acatlan.unam.mx>
2026-04-06 07:17:33 +0000

    4

--- nextjs-dashboard/app/dashboard/layout.tsx
+++ nextjs-dashboard/app/dashboard/layout.tsx
@@ -1,12 +1,17 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
- 
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-      <div className="w-full flex-none md:w-64">
+    <div className="flex h-screen flex-col md:flex-row">
+      {/* Sidebar */}
+      <div className="w-full md:w-64">
         <SideNav />
       </div>
-      <div className="grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+
+      {/* Contenido */}
+      <div className="flex-1 p-6">
+        {children}
+      </div>
     </div>
   );
 }
--- nextjs-dashboard/app/dashboard/sidenav.tsx
+++
@@ -1,7 +0,0 @@
-export default function SideNav() {
-  return (
-    <div className="p-4 bg-gray-200 h-full">
-      <p>SideNav</p>
-    </div>
-  );
-}
